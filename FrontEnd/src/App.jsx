c902b80f7896f2b96c53ca0c82078b75913c6415
narinderCanGit <narinder111198@gmail.com>
2023-04-17 14:37:45 +0530

    add navbar

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import AddTask from "./components/AddTask";
 import AddTaskClass from "./components/AddTaskClass";
 import Tasks from "./components/Tasks";
+import Navbar from './components/Navbar';
 
 const App = () => {
     return (
         <div>
+            <Navbar/>
             <AddTaskClass />
             <Tasks />
         </div>
---
+++ FrontEnd/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function ButtonAppBar() {
+    const { loginWithRedirect } = useAuth0();
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Home
+          </Typography>
+         <button color="inherit" onClick={() => loginWithRedirect()}>Log In</button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
