3777227d32926ff189f1f38eb9f21a83dbc58863
narinderCanGit <narinder111198@gmail.com>
2023-04-17 14:25:16 +0530

    Auth0 connection

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -4,12 +4,20 @@ import "./index.css";
 import App from "./App";
 import store from "./store/configureStore";
 import { Provider } from "react-redux";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    // <React.StrictMode>
-    <Provider store={store}>
-        <App />
-    </Provider>
-    // </React.StrictMode>
+    <Auth0Provider
+    domain="dev-xp18rvbf0xygbxhu.us.auth0.com"
+    clientId="IdHIlX3MbVhhoqBZRYUB5aVoodyNI8Xv"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+    >
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Auth0Provider>
+    
 );
