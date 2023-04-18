25c940d4d02155a4ae8036ea75b09f303f277d6f
narinderCanGit <narinder111198@gmail.com>
2023-04-18 14:40:42 +0530

    Implement Auth0

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import AddTask from "./components/AddTask";
 import AddTaskClass from "./components/AddTaskClass";
 import Tasks from "./components/Tasks";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const App = () => {
-    return (
-        <div>
-            <Navbar/>
-            <AddTaskClass />
-            <Tasks />
-        </div>
-    );
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  return (
+    <div>
+      <Navbar />
+      {isAuthenticated && (
+        <>
+          <AddTaskClass />
+          <Tasks />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function ButtonAppBar() {
-    const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -26,7 +26,25 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Home
           </Typography>
-         <button color="inherit" onClick={() => loginWithRedirect()}>Log In</button>
+
+          {isAuthenticated && (
+            <div >
+              <img style={{height:'50px', width:'50px'}} src={user.picture} alt={user.name} />
+              <p>{user.email}</p>
+            </div>
+          )}
+
+          {isAuthenticated ? (
+            <button
+              onClick={() => logout({ returnTo: window.location.origin })}
+            >
+              Log Out
+            </button>
+          ) : (
+            <button onClick={() => loginWithRedirect()}>Log In</button>
+          )}
+
+          {console.log(isAuthenticated)}
         </Toolbar>
       </AppBar>
     </Box>
